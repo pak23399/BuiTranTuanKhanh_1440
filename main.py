f24b30582cc67e944572e94a6885e53deec68bff
pham anh khoa-1537 <pak23399@gmail.com>
2026-03-11 08:47:56 +0700

    minus a ,b

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ a = float(input("Nhập số thứ nhất: "))
 b = float(input("Nhập số thứ hai: "))
 tong = a + b
 print("Tổng là:", tong)
+def minus(a, b):
+    return a - b
+if __name__ == "__main__":
+    a = 20  
+    b = 10
+    print("Difference of", a, "and", b, "is", minus(a, b))
